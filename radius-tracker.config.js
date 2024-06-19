47694919129eda53bda78cd68ab4aefc7a130275
Titus Okathe <titus.okathe@rangle.io>
2024-06-19 13:45:16 -0400

    feat: simplified workflow

--- radius-tracker.config.js
+++ radius-tracker.config.js
@@ -1,13 +1,47 @@
 const samples = [
     {
-        repoUrl: './tracker',
-        displayName: 'tracked-repository',
+        repoUrl: 'https://github.com/rangle/radius-token-tango',
+        displayName: 'token-tango',
         isTargetModuleOrPath: {
             grafana: /^@grafana\/ui(?!.*\.s?css$)/,
             mui: /^(@mui|@material-ui)/,
             headlessui: /^@headlessui/,
+            radixui: /^@radix-ui/,
         },
-        since: new Date("2020-01-01"),
+        since: new Date("2023-12-01"),
+    },
+    {
+        repoUrl: 'https://github.com/rangle/RN-UI-libs-POC',
+        displayName: 'rn-ui-libs-poc',
+        isTargetModuleOrPath: {
+            grafana: /^@grafana\/ui(?!.*\.s?css$)/,
+            mui: /^(@mui|@material-ui)/,
+            headlessui: /^@headlessui/,
+            radixui: /^@radix-ui/,
+        },
+        since: new Date("2023-12-01"),
+    },
+    {
+        repoUrl: 'https://github.com/rangle/RadiusWidgetPOC',
+        displayName: 'radius-widget-poc',
+        isTargetModuleOrPath: {
+            grafana: /^@grafana\/ui(?!.*\.s?css$)/,
+            mui: /^(@mui|@material-ui)/,
+            headlessui: /^@headlessui/,
+            radixui: /^@radix-ui/,
+        },
+        since: new Date("2023-12-01"),
+    },
+    {
+        repoUrl: 'https://github.com/adminazhar/shadcn-ui-template',
+        displayName: 'shadcn-ui-template',
+        isTargetModuleOrPath: {
+            grafana: /^@grafana\/ui(?!.*\.s?css$)/,
+            mui: /^(@mui|@material-ui)/,
+            headlessui: /^@headlessui/,
+            radixui: /^@radix-ui/,
+        },
+        since: new Date("2023-12-01"),
     }
 ]
 
